e5c2765577ffa1bc7eb8cf7bbaee82d1bf466607
cngen <osadebec01@gmail.com>
2026-07-31 06:16:03 +0000

    Remove space between hero title and dashboard button

--- web/app/components/auth/cards/card.tsx
+++ web/app/components/auth/cards/card.tsx
@@ -11,7 +11,7 @@ export const AuthButton = () => {
       return (
         <Link 
           href="/dashboard"
-          className="button-primary bg-white text-accent px-8 font-bold text-xl py-2 mt-5"
+          className="button-primary bg-white text-accent px-8 font-bold text-xl py-2"
         >
           DASHBOARD
         </Link>
@@ -20,7 +20,7 @@ export const AuthButton = () => {
       return (
           <Link 
             href="/auth/signin"
-            className="button-primary bg-white text-accent px-8 font-bold text-xl py-2 mt-5"
+            className="button-primary bg-white text-accent px-8 font-bold text-xl py-2"
           >
             GET STARTED
           </Link>
